d66641aac977b4e11f83f45197a4cfd23dca26fd
jkuanh Name <jkuanh@qq.com>
2025-04-06 19:51:09 +0800

    'v:1.0.4'

--- testing/settings.py
+++ testing/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 邮箱的配置
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # 使用SMTP服务器发送邮件
